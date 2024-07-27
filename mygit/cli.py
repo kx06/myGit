caa12036b96499e5bb1992ad4401a8ee122d2c28
kx <krishofficio0601@gmail.com>
2024-07-28 02:19:36 +0530

    added hash-object cat-file

--- mygit/cli.py
+++ mygit/cli.py
@@ -1,25 +1,72 @@
 import argparse
 import os
-from . import data
-
-
-def init(args):
-    data.init()
-    print(f"Initilalized an empty mygit repository in {os.getcwd()}/{data.GIT_DIR}")
+import sys
+from .data import MyGitFuncs
 
 
 def parse_args():
-    parser = argparse.ArgumentParser()
-
+    parser = argparse.ArgumentParser(
+        description="A simple Git-like version control system"
+    )
     commands = parser.add_subparsers(dest="command")
     commands.required = True
 
-    init_parser = commands.add_parser("init")
+    init_parser = commands.add_parser("init", help="Initialize a new repository")
     init_parser.set_defaults(func=init)
+
+    hash_object_parser = commands.add_parser(
+        "hash-object", help="Compute object ID and optionally create a blob from a file"
+    )
+    hash_object_parser.set_defaults(func=hash_object)
+    hash_object_parser.add_argument("file", help="File to hash")
+
+    cat_file_parser = commands.add_parser(
+        "cat-file", help="Provide content of repository object"
+    )
+    cat_file_parser.set_defaults(func=cat_file)
+    cat_file_parser.add_argument("object", help="The object to display")
 
     return parser.parse_args()
 
 
+def init(repo_funcs, args):
+    try:
+        repo_funcs.init()
+        print(
+            f"Initialized an empty mygit repository in {os.path.join(os.getcwd(), repo_funcs.GIT_DIR)}"
+        )
+    except Exception as e:
+        print(f"Error: {e}", file=sys.stderr)
+        sys.exit(1)
+
+
+def hash_object(repo_funcs, args):
+    # try:
+    with open(args.file, "rb") as f:
+        content = f.read()
+        print(repo_funcs.hash_object(content))
+    # except FileNotFoundError:
+    #     print(f"Error: File '{args.file}' not found", file=sys.stderr)
+    #     sys.exit(1)
+    # except Exception as e:
+    #     print(f"Error: {e}", file=sys.stderr)
+    #     sys.exit(1)
+
+
+def cat_file(repo_funcs, args):
+    try:
+        sys.stdout.flush()
+        sys.stdout.buffer.write(repo_funcs.get_object(args.object))
+    except Exception as e:
+        print(f"Error: {e}", file=sys.stderr)
+        sys.exit(1)
+
+
 def main():
+    repo_funcs = MyGitFuncs()
     args = parse_args()
-    args.func(args)
+    args.func(repo_funcs, args)
+
+
+if __name__ == "__main__":
+    main()
--- mygit/data.py
+++ mygit/data.py
@@ -1,7 +1,45 @@
-import os
+import hashlib
+from pathlib import Path
+from pathlib import PurePath
 
-GIT_DIR = ".mygit"
 
+class MyGitFuncs:
 
-def init():
-    os.makedirs(GIT_DIR)
+    GIT_DIR = ".mygit"
+
+    def __init__(self):
+        self.cwd = Path.cwd()
+        self.git_dir = self.cwd
+
+        def find_root(cwd):
+            root = PurePath(cwd).anchor
+            while cwd != root:
+                for i in Path.iterdir(cwd):
+                    if i.is_dir():
+                        if i.name == self.GIT_DIR:
+                            self.git_dir = cwd / self.GIT_DIR
+                            return True
+                    cwd = cwd.parent
+            return False
+
+        find_root(self.cwd)
+
+    def init(self):
+        if Path(self.GIT_DIR).exists():
+            raise Exception(f"repository already exists")
+        Path(self.GIT_DIR).mkdir()
+        Path(PurePath(self.GIT_DIR, "objects").joinpath()).mkdir()
+
+    def hash_object(self, data):
+        oid = hashlib.sha256(data).hexdigest()
+        path = self.git_dir / "objects" / oid
+        with path.open(mode="wb") as f:
+            f.write(data)
+        return oid
+
+    def get_object(self, oid):
+        path = self.git_dir / "objects" / oid
+        if not path.exists():
+            raise Exception(f"object {oid} not found")
+        with path.open(mode="rb") as f:
+            return f.read()
